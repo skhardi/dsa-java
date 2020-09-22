ae6f57482748cd861c5fc0d8143050c150c302b3
Soph <skhardi@emory.edu>
2020-09-21 21:14:45 -0500

    Quiz 2

---
+++ src/main/java/edu/emory/cs/queue/BinaryHeap.java
@@ -0,0 +1,70 @@
+package edu.emory.cs.queue;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class BinaryHeap<K extends Comparable<K>> extends AbstractPQ<K> {
+    protected final List<K> keys;
+    private int size = 0;
+
+    BinaryHeap() {
+        super(Comparator.naturalOrder());
+        keys = new ArrayList<>();
+        keys.add(null);
+    }
+
+    BinaryHeap(Comparator<K> priority) {
+        super(priority);
+        keys = new ArrayList<>();
+        keys.add(null);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void add(K key) {
+        keys.add(key);
+        swim(++size);
+    }
+
+    @Override
+    public K remove() {
+        if (isEmpty())
+            throw new NoSuchElementException("Empty List");
+
+        swap(1, size);
+        K removed = keys.remove(size--);
+        sink();
+
+        return removed;
+    }
+
+    private void swim(int index) {
+        for (; index > 1 && compare(index, index/2) > 0; index /=2) {
+                swap(index,index/2);
+        }
+    }
+
+    private void sink() {
+        for (int k = 1, i = 2; i <= size; k = i, i *= 2) {
+            if (i < size && compare(i, i + 1) < 0) i++;
+            if (compare(k, i) >= 0) break;
+            swap(k, i);
+        }
+    }
+
+    private void swap(int index1, int index2) {
+        K tmp = keys.get(index1);
+        keys.set(index1, keys.get(index2));
+        keys.set(index2, tmp);
+    }
+
+    private int compare(int index1, int index2) {
+        return priority.compare(keys.get(index1), keys.get(index2));
+    }
+}
